73982f174262a05d103efba22c175b8618d47be4
mtxribeiro <matheusribeirovivas06@gmail.com>
2025-10-27 01:23:35 -0300

    commit 1

---
+++ .bashrc
@@ -0,0 +1,21 @@
+[[ $- != *i* ]] && return
+
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+
+alias ll='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+
+alias rm='rm -r'
+alias rmf='rm -rf'
+
+alias cp='cp -r'
+
+alias cl='clear'
+
+alias sp='sudo pacman'
+
+alias vim='nvim'
+
+PS1='\w '
---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Atualização e instalação de pacotes
+sudo pacman -Syu --noconfirm
+sudo pacman -S --needed --noconfirm \
+  gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav \
+  pipewire-audio \
+  gtk3 gtk4 qt5-wayland qt6-wayland xorg-xwayland \
+  xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-gtk xdg-utils polkit-kde-agent gnome-keyring \
+  xdg-user-dirs xdg-user-dirs-gtk \
+  noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-liberation ttf-firacode-nerd ttf-jetbrains-mono-nerd \
+  git base-devel flatpak zip unzip unrar mpv imv btop fastfetch pavucontrol kitty hyprland hyprpaper waybar nvim firefox
+
+# Instalação do yay se necessário
+command -v yay &>/dev/null || (
+  cd /tmp &&
+    git clone https://aur.archlinux.org/yay.git &&
+    cd yay &&
+    makepkg -si --noconfirm &&
+    cd "$HOME" &&
+    rm -rf /tmp/yay
+)
+
+# Instalação de pacotes AUR
+yay -S --needed --noconfirm hyprshot tofi
+
+# Atualização dos diretórios do usuário
+xdg-user-dirs-update
+
+# Cache de fontes
+fc-cache -rv
+
+# Limpeza do cache do pacman
+sudo pacman -Scc --noconfirm
+
+# Copiando configurações do repositório
+REPO_DIR="$HOME/desktop"
+
+# .bashrc
+cp "$REPO_DIR/.bashrc" "$HOME/.bashrc"
+
+# .config
+cp -r "$REPO_DIR/.config" "$HOME/.config"
+
+# Wallpapers
+mkdir -p "$HOME/Imagens"
+cp -r "$REPO_DIR/Wallpapers" "$HOME/Imagens/"
+
+# Mensagem final
+echo -e "\n\033[1;32mInstalação concluída. Por favor, reinicie o sistema.\033[0m"
