1eaabf70e17a2fe032b85271c2aadaf64220f237
Matheus Ribeiro <matheusribeirovivas06@gmail.com>
2025-11-05 01:04:58 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo pacman -S --needed --noconfirm \
   gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-libav \
   pipewire-audio \
   gtk3 gtk4 qt5-wayland qt6-wayland xorg-xwayland \
-  xdg-desktop-portal xdg-desktop-portal-hyprland xdg-user-dirs \
+  xdg-desktop-portal-hyprland xdg-user-dirs \
   polkit-gnome gnome-keyring \
   noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-liberation ttf-jetbrains-mono-nerd \
   git base-devel flatpak zip unzip unrar mpv imv vim fastfetch xfce4-taskmanager \
