8edd3926064f7a1c90f17f7c5908bc737fb3e0a4
Matheus Ribeiro <matheusribeirovivas06@gmail.com>
2025-11-05 01:01:11 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo pacman -S --needed --noconfirm \
   gtk3 gtk4 qt5-wayland qt6-wayland xorg-xwayland \
   xdg-desktop-portal xdg-desktop-portal-hyprland xdg-user-dirs \
   polkit-gnome gnome-keyring \
-  noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-liberation ttf-firacode-nerd ttf-jetbrains-mono-nerd \
+  noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-liberation ttf-jetbrains-mono-nerd \
   git base-devel flatpak zip unzip unrar mpv imv vim fastfetch xfce4-taskmanager \
   hyprland hyprpaper waybar materia-gtk-theme pavucontrol thunar gvfs kitty xed
 command -v yay &>/dev/null || (cd /tmp && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm && cd "$HOME" && rm -rf /tmp/yay)
